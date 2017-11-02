97fb62d354b5a1571b6497862be6d26a3636f9d5
Michal Kalita <kalita.michal@gmail.com>
2017-11-02 23:42:29 +0100

    Added dynamic functions

--- index.js
+++ index.js
@@ -1,54 +1,90 @@
 const fs = require('fs')
 const path = require('path')
 
-const args = process.argv.splice(process.execArgv.length + 2)
+const args = process.argv.splice(2)
 
-function readFile(file, pwd = '') {
-  const filePath = path.resolve(pwd, file)
-  try {
-    const content = fs.readFileSync(filePath)
-    return content.toString()
-  } catch (error) {
-    throw new Error(`File '${filePath}' not found`)
+function TextScript(_file) {
+  this.file = _file
+  this.commands = {}
+
+  this.commands.import = (input) => {
+    const moduleName = input.command.replace(/^import/i, '').trim()
+
+    let name = /^'(.*)'$/.exec(moduleName)
+    name = name === null ? moduleName : name[1]
+
+    const absolutePath = path.resolve(path.dirname(input.file), `${name}.js`)
+
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    require(absolutePath)()
   }
-}
 
-function commandImport(module, sourceFile) {
-  let name = /^'(.*)'$/.exec(module)
-  name = name === null ? module : name[1]
+  this.readFile = (file, pwd = '') => {
+    const filePath = path.resolve(pwd, file)
+    try {
+      return fs.readFileSync(filePath).toString()
+    } catch (error) {
+      throw new Error(`File '${filePath}' not found`)
+    }
+  }
 
-  const absolutePath = path.resolve(path.dirname(sourceFile), `${name}.js`)
-
-  // eslint-disable-next-line import/no-dynamic-require, global-require
-  require(absolutePath)()
-}
-
-function parseFile(file) {
-  const content = readFile(file)
-
-  /*
-  header << headers
-  header2
-  ------- << separator
-  Go one
-  Go two << commands
-  */
-
-  // block processing
-  content.split('\n\n')
-    .map(i => i.trim())
-    .forEach((block) => {
-      // command processing
-      block.split('\n')
-        .map(i => i.trim())
-        .forEach((command) => {
-          if (command.toLowerCase().startsWith('import')) {
-            commandImport(command.replace(/^import/i, '').trim(), file)
-          } else {
-            console.log(command)
-          }
-        })
+  this.addCommand = (headers, commands, file) => {
+    const headerArray = headers.constructor === Array ? headers : [headers]
+    headerArray.forEach((name) => {
+      this.commands[name.toLowerCase()] = () => this.processBlock(commands, file)
     })
+  }
+
+  /**
+   * @argument {string} name
+   * @argument {string} file
+   */
+  this.runCommand = (name, file) => {
+    const searchName = `${name.toLowerCase()} `
+    const possibleCommands = Object.keys(this.commands)
+      .filter(c => searchName.startsWith(`${c} `)) // commands starts with same words
+
+    if (possibleCommands.length === 0) {
+      throw new Error(`Command '${name}' not found`)
+    }
+    const command = possibleCommands.length === 1 ? possibleCommands[0] :
+      possibleCommands.reduce((a, b) => ((a.length > b.length) ? a : b))
+
+    this.commands[command]({
+      file,
+      command: name,
+    })
+  }
+
+  /**
+   * Process lines, create function or run commands on lines
+   */
+  this.processBlock = (lines, file) => {
+    const separatorLine = lines.findIndex(i => /^---/.test(i))
+    if (separatorLine > 0) {
+      const headers = lines.slice(0, separatorLine)
+      this.addCommand(headers, lines.slice(separatorLine + 1), file)
+    } else {
+      lines.forEach(command => this.runCommand(command, file))
+    }
+  }
+
+  this.parseFile = (file) => {
+    const content = this.readFile(file)
+
+    // block processing
+    content.split('\n\n')
+      .map(i => i.trim())
+      .forEach((block) => {
+        const lines = block.split('\n').map(i => i.trim())
+
+        this.processBlock(lines, file)
+      })
+  }
+
+  this.parseFile(this.file)
 }
 
-parseFile(args[0])
+TextScript(args[0])
+
+module.exports = TextScript
